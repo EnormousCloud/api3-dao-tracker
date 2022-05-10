5d6657ab022e185b5231d52b780d74a4e65e361c
EnormousCloud <enormous@webcerebrium.com>
2022-05-10 09:51:54 +0300

    metrics clean up

--- server/src/metrics.rs
+++ server/src/metrics.rs
@@ -3,79 +3,88 @@ use client::state::AppState;
 use lazy_static::lazy_static;
 use prometheus::{opts, register_gauge, register_int_gauge};
 use prometheus::{Encoder, Gauge, IntGauge, Opts, Registry, TextEncoder};
-use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 lazy_static! {
-    pub static ref CHAIN_ID_GAUGE: IntGauge =
-        register_int_gauge!(opts!("chain_id", "Chain ID that is being syncing",)).unwrap();
-    pub static ref WATCHING: IntGauge =
-        register_int_gauge!(opts!("watching", "Whether the chain is being watched",)).unwrap();
-    pub static ref BLOCK_START_GAUGE: IntGauge =
-        register_int_gauge!(opts!("block_start", "Number of block that is being synced",)).unwrap();
-    pub static ref BLOCK_END_GAUGE: IntGauge =
-        register_int_gauge!(opts!("block_end", "Number of block that is being synced",)).unwrap();
+    pub static ref UP: IntGauge = register_int_gauge!(opts!("up", "Server is running")).unwrap();
 }
 
 lazy_static! {
-    pub static ref NUM_ADDRESSES: IntGauge =
-        register_int_gauge!(opts!("addresses", "Number of addresses in DAO",)).unwrap();
-    pub static ref NUM_VOTINGS: IntGauge =
-        register_int_gauge!(opts!("votings", "Number of votings in DAO",)).unwrap();
-    pub static ref EPOCHS: IntGauge =
-        register_int_gauge!(opts!("epochs", "Number of epochs",)).unwrap();
-    pub static ref EPOCH_INDEX: IntGauge =
-        register_int_gauge!(opts!("epoch_index", "Index of the current epoch",)).unwrap();
-    pub static ref APR: Gauge =
-        register_gauge!(opts!("apr", "Index of the current epoch",)).unwrap();
-    pub static ref LAST_BLOCK: IntGauge =
-        register_int_gauge!(opts!("last_block", "Index of the last block",)).unwrap();
-    pub static ref GENESIS_APR: Gauge =
-        register_gauge!(opts!("genesis_apr", "Genesis APR",)).unwrap();
-    pub static ref MIN_APR: Gauge = register_gauge!(opts!("min_apr", "Min APR",)).unwrap();
-    pub static ref MAX_APR: Gauge = register_gauge!(opts!("max_apr", "Max APR",)).unwrap();
-    pub static ref STAKE_TARGET: Gauge =
-        register_gauge!(opts!("stake_target", "Stake Target",)).unwrap();
-    pub static ref UNSTAKE_WAIT_PERIOD: IntGauge =
-        register_int_gauge!(opts!("unstake_wait_period", "Unstake Wait Period",)).unwrap();
-    pub static ref REWARD_PERIOD: IntGauge =
-        register_int_gauge!(opts!("reward_vesting_period", "Reward Vesting Period",)).unwrap();
-    pub static ref CIRCULATING_SUPPLY: Gauge =
-        register_gauge!(opts!("circulating_supply", "API3 Circulating Supply",)).unwrap();
-    pub static ref TOTAL_SUPPLY: Gauge =
-        register_gauge!(opts!("total_supply", "API3 Total Supply",)).unwrap();
-    pub static ref LOCKED_GOV: Gauge =
-        register_gauge!(opts!("locked_by_governance", "Locked By Governance",)).unwrap();
-    pub static ref LOCKED_REWARDS: Gauge =
-        register_gauge!(opts!("locked_rewards", "Locked Rewards",)).unwrap();
-    pub static ref LOCKED_VESTINGS: Gauge =
-        register_gauge!(opts!("locked_vestings", "Locked Vestings",)).unwrap();
-    pub static ref TIME_LOCKED: Gauge =
-        register_gauge!(opts!("time_locked", "Time Locked",)).unwrap();
-    pub static ref TOTAL_LOCKED: Gauge =
-        register_gauge!(opts!("total_locked", "Total Locked",)).unwrap();
+    pub static ref CHAIN_ID_GAUGE: IntGauge =
+        register_int_gauge!(opts!("sync_chain_id", "Chain ID that is being syncing",)).unwrap();
+    pub static ref WATCHING: IntGauge =
+        register_int_gauge!(opts!("sync_watching", "Whether the chain is being watched",)).unwrap();
+    pub static ref BLOCK_START_GAUGE: IntGauge = register_int_gauge!(opts!(
+        "sync_block_start",
+        "Number of block that is being synced",
+    ))
+    .unwrap();
+    pub static ref BLOCK_END_GAUGE: IntGauge = register_int_gauge!(opts!(
+        "sync_block_end",
+        "Number of block that is being synced",
+    ))
+    .unwrap();
+}
+
+lazy_static! {
     pub static ref SINCE_LAST_UPDATE: IntGauge = register_int_gauge!(opts!(
-        "since_last_update",
+        "sync_since_last_update",
         "Seconds since the last state update",
     ))
     .unwrap();
+    pub static ref NUM_ADDRESSES: IntGauge =
+        register_int_gauge!(opts!("dao_addresses", "Number of addresses in DAO",)).unwrap();
+    pub static ref NUM_VOTINGS: IntGauge =
+        register_int_gauge!(opts!("dao_votings", "Number of votings in DAO",)).unwrap();
+    pub static ref EPOCHS: IntGauge =
+        register_int_gauge!(opts!("dao_epochs", "Number of epochs",)).unwrap();
+    pub static ref EPOCH_INDEX: IntGauge =
+        register_int_gauge!(opts!("dao_epoch_index", "Index of the current epoch",)).unwrap();
+    pub static ref APR: Gauge =
+        register_gauge!(opts!("dao_apr", "Index of the current epoch",)).unwrap();
+    pub static ref LAST_BLOCK: IntGauge =
+        register_int_gauge!(opts!("dao_last_block", "Index of the last block",)).unwrap();
+    pub static ref GENESIS_APR: Gauge =
+        register_gauge!(opts!("dao_genesis_apr", "Genesis APR",)).unwrap();
+    pub static ref MIN_APR: Gauge = register_gauge!(opts!("min_apr", "Min APR",)).unwrap();
+    pub static ref MAX_APR: Gauge = register_gauge!(opts!("max_apr", "Max APR",)).unwrap();
+    pub static ref STAKE_TARGET: Gauge =
+        register_gauge!(opts!("dao_stake_target", "Stake Target",)).unwrap();
+    pub static ref UNSTAKE_WAIT_PERIOD: IntGauge =
+        register_int_gauge!(opts!("dao_unstake_wait_period", "Unstake Wait Period",)).unwrap();
+    pub static ref REWARD_PERIOD: IntGauge =
+        register_int_gauge!(opts!("dao_reward_vesting_period", "Reward Vesting Period",)).unwrap();
+    pub static ref CIRCULATING_SUPPLY: Gauge =
+        register_gauge!(opts!("dao_circulating_supply", "API3 Circulating Supply",)).unwrap();
+    pub static ref TOTAL_SUPPLY: Gauge =
+        register_gauge!(opts!("dao_total_supply", "API3 Total Supply",)).unwrap();
+    pub static ref LOCKED_GOV: Gauge =
+        register_gauge!(opts!("dao_locked_by_governance", "Locked By Governance",)).unwrap();
+    pub static ref LOCKED_REWARDS: Gauge =
+        register_gauge!(opts!("dao_locked_rewards", "Locked Rewards",)).unwrap();
+    pub static ref LOCKED_VESTINGS: Gauge =
+        register_gauge!(opts!("dao_locked_vestings", "Locked Vestings",)).unwrap();
+    pub static ref TIME_LOCKED: Gauge =
+        register_gauge!(opts!("dao_time_locked", "Time Locked",)).unwrap();
+    pub static ref TOTAL_LOCKED: Gauge =
+        register_gauge!(opts!("dao_total_locked", "Total Locked",)).unwrap();
     pub static ref SINCE_LAST_VOTINGS: IntGauge = register_int_gauge!(opts!(
-        "since_last_votings_read",
+        "dao_since_last_votings_read",
         "Seconds since the last votings read",
     ))
     .unwrap();
     pub static ref SINCE_LAST_ENS: IntGauge = register_int_gauge!(opts!(
-        "since_last_ens_read",
+        "dao_since_last_ens_read",
         "Seconds since the last ENS read",
     ))
     .unwrap();
     pub static ref SINCE_LAST_TREASURIES: IntGauge = register_int_gauge!(opts!(
-        "since_last_treasuries_read",
+        "dao_since_last_treasuries_read",
         "Seconds since the last treasuries read",
     ))
     .unwrap();
     pub static ref SINCE_LAST_CIRCULATION: IntGauge = register_int_gauge!(opts!(
-        "since_last_circulation_read",
+        "dao_since_last_circulation_read",
         "Seconds since the last circulation read",
     ))
     .unwrap();
@@ -83,8 +92,9 @@ lazy_static! {
 
 pub fn handler(state: &AppState) -> String {
     let encoder = TextEncoder::new();
-    let labels = HashMap::new();
-    let sr = Registry::new_custom(Some("dao".to_string()), Some(labels)).unwrap();
+    let sr = Registry::new();
+    sr.register(Box::new(UP.clone())).unwrap();
+    sr.register(Box::new(SINCE_LAST_UPDATE.clone())).unwrap();
     sr.register(Box::new(NUM_ADDRESSES.clone())).unwrap();
     sr.register(Box::new(NUM_VOTINGS.clone())).unwrap();
     sr.register(Box::new(CHAIN_ID_GAUGE.clone())).unwrap();
@@ -109,7 +119,6 @@ pub fn handler(state: &AppState) -> String {
     sr.register(Box::new(TIME_LOCKED.clone())).unwrap();
     sr.register(Box::new(TOTAL_LOCKED.clone())).unwrap();
     // seconds, passed since various events
-    sr.register(Box::new(SINCE_LAST_UPDATE.clone())).unwrap();
     sr.register(Box::new(SINCE_LAST_VOTINGS.clone())).unwrap();
     sr.register(Box::new(SINCE_LAST_ENS.clone())).unwrap();
     sr.register(Box::new(SINCE_LAST_TREASURIES.clone()))
@@ -177,6 +186,7 @@ pub fn handler(state: &AppState) -> String {
         });
     }
 
+    UP.set(1);
     let mut buffer = Vec::<u8>::new();
     encoder.encode(&sr.gather(), &mut buffer).unwrap();
     String::from_utf8(buffer.clone()).unwrap()
@@ -184,12 +194,13 @@ pub fn handler(state: &AppState) -> String {
 
 pub fn syncing() -> String {
     let encoder = TextEncoder::new();
-    let labels = HashMap::new();
-    let sr = Registry::new_custom(Some("sync".to_string()), Some(labels)).unwrap();
+    let sr = Registry::new();
+    sr.register(Box::new(UP.clone())).unwrap();
     sr.register(Box::new(CHAIN_ID_GAUGE.clone())).unwrap();
     sr.register(Box::new(WATCHING.clone())).unwrap();
     sr.register(Box::new(BLOCK_START_GAUGE.clone())).unwrap();
     sr.register(Box::new(BLOCK_END_GAUGE.clone())).unwrap();
+    UP.set(0);
     let mut buffer = Vec::<u8>::new();
     encoder.encode(&sr.gather(), &mut buffer).unwrap();
     String::from_utf8(buffer.clone()).unwrap()
